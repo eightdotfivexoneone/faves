eabf6d0d5aa48ae5a9c31f07e93acf312b6af920
Steven Howard <steven@smhoward.net>
2018-05-30 14:20:17 -0400

    works with products db

--- CodeAlongJoin/app.js
+++ CodeAlongJoin/app.js
@@ -23,13 +23,13 @@ var connection = mysql.createConnection({
 
   // Your password
   password: "qwertyroot71",
-  database: "animals_db"
+  database: "bamazon_app"
 });
 
 
 
 app.get("/", (req, results) => {
-  var q = "SELECT COUNT(*) AS count FROM favorite_songs";
+  var q = "SELECT COUNT(*) AS count FROM products";
   connection.query(q, function(err, res) {
     if (err) throw err;
     // console.log(res[0]);
@@ -40,33 +40,24 @@ app.get("/", (req, results) => {
   });
 });
 
-app.get("/joke", (req, results) => {
-  var joke = "how can you tell when..."
-  results.send(joke);
-//   var q = "SELECT COUNT(*) AS count FROM favorite_songs";
-//   connection.query(q, function(err, res) {
-//     if (err) throw err;
-//     // console.log(res[0]);
-//     var count = res[0].count;
-//     // results.send('<h1>You have ' + count +  ' favorite songs.</h1>');
-//     // connection.end();
-//     results.render('home', {data: count});  
-//   });
-});
+
 
 app.post("/register", (request, results) => {
 var track = {
-  song: request.body.song,
-  artis: request.body.artis,
-  score: request.body.score,
-  genre: request.body.genre
+  product_name: request.body.product_name,
+  image_url: request.body.image_url,
+  product_desc: request.body.product_desc,
+  department_name: request.body.department_name,
+  instock_quantity: request.body.instock_quantity,
+  consumer_price: request.body.consumer_price,
+  business_cost: request.body.business_cost
 };
   console.log(JSON.stringify(request.body));
   // var s = {email: req.body.email};
   // console.log("Post Request Sent to Register", req.body);
   // var qinsert = "INSERT INTO favorite_songs (song, artis, score, genre) VALUES("+ track.title +", "+ track.artist +", " + track.sc + "," + track.g + ")";
   
-  var sql = `INSERT INTO favorite_songs SET ?`;
+  var sql = `INSERT INTO products SET ?`;
   let query = connection.query(sql, track, (err, result) => {
     if(err){
       throw err;
@@ -75,10 +66,13 @@ var track = {
   })
   // 
   results.render('test', {data:5, 
-                          song: request.body.song,
-                          artis: request.body.artis,
-                          score: request.body.score,
-                          genre: request.body.genre } )
+    product_name: request.body.product_name,
+    image_url: request.body.image_url,
+    product_desc: request.body.product_desc,
+    department_name: request.body.department_name,
+    instock_quantity: request.body.instock_quantity,
+    consumer_price: request.body.consumer_price,
+    business_cost: request.body.business_cost } )
 });
 /*
 router.get('/data', function(req, res){
